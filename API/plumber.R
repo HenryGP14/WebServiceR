6eb54eedfe49ccfdd5dddd96762f637fa611563a
CarlosAlmeida2000 <carlos.almeida2017@uteq.edu.ec>
2021-08-15 22:25:49 -0500

    Creando los métodos GET para obtener las todas provincias o por id, nombres y obtener histogramas de los datos cifras del COVID19

--- API/plumber.R
+++ API/plumber.R
@@ -1,26 +1,61 @@
-# Instalar el paquete plumber
+# INSTALACION DE PAQUETES
 # install.packages("plumber")
+# install.packages("RPostgreSQL")
+
+# CARGA DE PAQUETES
 library(plumber)
+library(RPostgreSQL)
+library(DBI)
+
+get_conectBD = function(){
+  driver <- RPostgreSQL::PostgreSQL()
+  dbname <- "contagios_covid"
+  url_host <- "localhost"
+  port <- 5432
+  password <- "123456"
+  conexion_BD <- dbConnect(driver, dbname = dbname, host = url_host, port = port, user = "postgres", password = password) 
+  return (conexion_BD)
+}
 
 #* Echo back the input
-#* @param msg The message to echo
-#* @get /echo
-function(msg = "") {
-  list(msg = paste0("The message is: '", msg, "'"))
+#* @get /provincias
+function() {
+  query <- "SELECT * from provincia"
+  connection <- get_conectBD()
+  provincias <- dbGetQuery(connection, query)
+  dbDisconnect(connection)
+  provincias
+}
+
+#* Echo back the input
+#* @param id obtener provincia por id
+#* @get /provincia_id
+function(id = "") {
+  query <- paste0("SELECT * from provincia WHERE public.provincia.idprovincia=","'",id,"'")
+  connection <- get_conectBD()
+  provincias <- dbGetQuery(connection, query)
+  dbDisconnect(connection)
+  provincias
+}
+
+#* Echo back the input
+#* @param name obtener provincia por nombre
+#* @get /provincia_name
+function(name = "") {
+  query <- paste0("SELECT * from provincia WHERE public.provincia.provincia=","'",name,"'")
+  connection <- get_conectBD()
+  provincias <- dbGetQuery(connection, query)
+  dbDisconnect(connection)
+  provincias
 }
 
 #* Plot a histogram
 #* @serializer png
-#* @get /plot
-function() {
-  rand <- rnorm(100)
-  hist(rand)
-}
-
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-function(a, b) {
-  as.numeric(a) + as.numeric(b)
+#* @get /plot_provincia
+function(campo = "") {
+  query <- paste0("SELECT * from provincia")
+  connection <- get_conectBD()
+  provincias <- dbGetQuery(connection, query)
+  dbDisconnect(connection)
+  hist(as.numeric(provincias[,campo]))
 }
