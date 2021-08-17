bb197933d851d33480ad2a96150dee968f5e608e
johnplazarte18 <john.plazarte2017@uteq.edu.ec>
2021-08-16 19:30:03 -0500

    Funcionalidad javascript y html

--- Frontend/js/code.js
+++ Frontend/js/code.js
@@ -1,24 +1,31 @@
 var cmbCampos = document.getElementById('cmbCampos');
-var parametro = document.getElementById('parametro');
+var imagenPlot = document.getElementById('imgPlot');
+var contentData= document.getElementById('contentData');
+var lista_plot= document.getElementById('lista_plot');
+getProvincias();
 cmbCampos.onchange = function(e){
     if(cmbCampos.value !== "todas_las_provincias"){
-        parametro.type = "text"
+        getProvinciaId(cmbCampos.value);
+        contentData.style.display = 'block';
+        imagenPlot.style.display = 'none';
+        lista_plot.style.display = 'none';
     }
-    else{
-        parametro.type = "hidden"
+    else {
+        contentData.style.display = 'none';
+        imagenPlot.style.display = 'block';
+        lista_plot.style.display = 'block';
     }
 }
 
-getProvincias();
-getProvinciaId(5);
-getProvinciaName('Azuay');
-getProvinciaPlot('contagiados');
 function getProvincias(){
     fetch('http://127.0.0.1:3731/provincias')
     .then(response => response.json())
     .then(json =>{
         json.forEach(element => {
-            console.log(element);//aqui llenas el combo
+            var option = document.createElement("option");
+            option.value=element.idprovincia;
+            option.text=element.provincia
+            cmbCampos.appendChild(option);
         });
     })
     
@@ -27,17 +34,16 @@ function getProvinciaId(id){
     fetch('http://127.0.0.1:3731/provincia_id?id='+id)
     .then(response => response.json())
     .then(json =>{
-        console.log(json[0]);
-    })
-}
-function getProvinciaName(name){
-    fetch('http://127.0.0.1:3731/provincia_name?name='+name)
-    .then(response => response.json())
-    .then(json =>{
-        console.log(json[0]);
+        contentData.innerHTML="<b>"+json[0].provincia+"</b><br>"+
+        "superficie: "+json[0].superficie+"<br>"+
+        "hombres: "+json[0].hombres+"<br>"+
+        "mujeres: "+json[0].mujeres+"<br>"+
+        "problacion_total: "+json[0].problacion_total+"<br>"+
+        "contagiados: "+json[0].contagiados+"<br>"+
+        "recuperados: "+json[0].recuperados+"<br>"+
+        "fallecidos: "+json[0].fallecidos+"<br>";
     })
 }
 function getProvinciaPlot(campo){
-    document.getElementById('imgPlot')
-    .setAttribute("src", "http://127.0.0.1:3731/plot_provincia?campo="+campo);
+    imagenPlot.setAttribute("src", "http://127.0.0.1:3731/plot_provincia?campo="+campo);
 }
